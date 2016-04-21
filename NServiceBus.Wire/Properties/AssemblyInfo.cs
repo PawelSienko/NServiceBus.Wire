088bf91046fe3be08562e7a355cd3119d0dc211c
Igal Tabachnik <hmemcpy@gmail.com>
2016-04-21 13:26:49 +0300

    Setting the correct version number

--- NServiceBus.Wire/Properties/AssemblyInfo.cs
+++ NServiceBus.Wire/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("NServiceBus.Wire")]
 [assembly: AssemblyProduct("NServiceBus.Wire")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.1")]
+[assembly: AssemblyFileVersion("2.0.1")]
